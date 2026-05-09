ed65e891d41cae161c710dde4fdfaf4c1117b661
Rextrading <rextrading2000@gmail.com>
2026-05-09 23:37:16 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Remake from './remakes/may/re1/App'
+import Remake from './remakes/may/re4/re2/Remake'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
